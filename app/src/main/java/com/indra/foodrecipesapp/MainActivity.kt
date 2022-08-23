706d30d81bc9679d745a0505aa8914d789209c7e
arthur3133 <alanprajapati8624@gmail.com>
2022-08-23 12:53:36 +0530

    1. Designed navigation graph. 2. Added 3 fragments in navigation graph. 3. Connected 3 fragments to bottom navigation view.

--- app/src/main/java/com/indra/foodrecipesapp/MainActivity.kt
+++ app/src/main/java/com/indra/foodrecipesapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.indra.foodrecipesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.indra.foodrecipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.recipesFragment,
+            R.id.favoritesFragment,
+            R.id.foodJokeFragment
+        ))
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
