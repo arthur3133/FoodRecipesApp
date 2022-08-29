2c51c2b706d91d78fa2290d2ea1fa2dc5dd21f34
arthur3133 <alanprajapati8624@gmail.com>
2022-08-29 10:30:26 +0530

    Designed Recipes Bottom Sheet.

---
+++ app/src/main/java/com/indra/foodrecipesapp/ui/fragment/recipes/RecipesBottomSheet.kt
@@ -0,0 +1,18 @@
+package com.indra.foodrecipesapp.ui.fragment.recipes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.indra.foodrecipesapp.R
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+    }
+}
