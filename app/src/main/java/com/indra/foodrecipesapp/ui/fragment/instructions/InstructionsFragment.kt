1f758fc24144d8c4fcb1ce57b81cf7fc8ed3191d
arthur3133 <alanprajapati8624@gmail.com>
2022-09-01 11:32:02 +0530

    1. Design Instructions fragment. 2. Load data in Instructions fragment.

--- app/src/main/java/com/indra/foodrecipesapp/ui/fragment/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/indra/foodrecipesapp/ui/fragment/instructions/InstructionsFragment.kt
@@ -1,19 +1,36 @@
 package com.indra.foodrecipesapp.ui.fragment.instructions
 
+import android.content.ContentResolver
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.indra.foodrecipesapp.R
+import com.indra.foodrecipesapp.databinding.FragmentInstructionsBinding
+import com.indra.foodrecipesapp.models.Result
+import com.indra.foodrecipesapp.util.Constants
 
 class InstructionsFragment : Fragment() {
-
+    private var _binding:FragmentInstructionsBinding?=null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+        val result: Result? = arguments?.getParcelable(Constants.RECIPE_RESULT_KEY)
+        result?.sourceUrl?.let {
+            binding.webView.webViewClient = object : WebViewClient(){}
+            binding.webView.loadUrl(it)
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
